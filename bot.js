ad1e69d590941bd3520b7419463ffd9b4ad68554
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-09 14:34:35 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require('fs')
+const YTDL = require("ytdl-core")
+let musicplaying = false;
+console.log(`${musicplaying}`);
+var servers = {};
+queue: []
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.generateInvite(["ADMINISTRATOR"])
+  .then(link => {
+    console.log(`Generated bot invite link: ${link}`);
+  });
+  let tempguild = client.guilds.find(`name`, `Coruscant Guard`);
+  console.log(`Guild "${tempguild.name}" was found.`);
+  let temprole = tempguild.roles.find('name', 'CAR: 401st Discord Manager');
+  console.log(`Role "${temprole.name}" was found.`);
+  if (temprole.hoist === true) {
+    console.log('Hoist is active.');
+    return;
+  }
+  if (!temprole.hoist === true) {
+    temprole.hoist = true;
+    console.log(temprole.hoist);
+  }
+  if(!tempguild.channels.find(`name`, `messagelogs`)) {
+    console.log(`Unable to find messagelogs channel.`);
+    tempguild.createChannel(`messagelogs`, `text`);
+    console.log(`Created messagelogs channel.`);
+    return;
+  }
+  if(tempguild.channels.find(`name`, `messagelogs`)) {
+    console.log(`messasgelogs channel was found.`);
+  }
+  client.user.setGame('with the wonderful Coruscant Guard.');
+});
+client.on('messageUpdate', function(oldmsg, newmsg) {
+  let channel = newmsg.channel.guild.channels.find('name', 'messagelogs');
+  let person = newmsg.author.username;
+  channel.sendMessage(`${person} edited a message.`);
+  channel.sendMessage(`Before: ${oldmsg}`);
+  channel.sendMessage(`After: ${newmsg}`);
+  channel.sendMessage('______________');
+  console.log(`${person} edited a message.`);
+  console.log(`Before: ${oldmsg}`);
+  console.log(`After: ${newmsg}`);
+  console.log('______________');
+});
+client.login(process.env.BOT_TOKEN);
