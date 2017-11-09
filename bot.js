0845c2f926de594a0387819f8afa8d78acad2f50
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-09 13:39:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs')
 const YTDL = require("ytdl-core")
+let playing = false
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.generateInvite(["ADMINISTRATOR"])
