9b898d7c410f5aad771b0f2b3ab0bdd53bbcdcd7
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-09 15:54:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const fs = require('fs');
 const YTDL = require("ytdl-core");
 const PREFIX = "-401-";
+require("FFmpeg")
 let musicplaying = false;
 console.log(`${musicplaying}`);
 var servers = {};
