fa957863e85a47aeda5ff2529826195cee0f47ce
XavierTG <33395394+XavierTG@users.noreply.github.com>
2018-07-27 11:04:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,6 @@ const fs = require('fs');
 const YTDL = require("ytdl-core");
 const PREFIX = "DataCommand-";
 const opusscript = require("opusscript");
-const FFMPEG = require('fluent-ffmpeg');
-const axios = require('axios');
-console.log(`secret-key: ${secret}`)
-let musicplaying = false;
-var servers = {};
+
 
 
