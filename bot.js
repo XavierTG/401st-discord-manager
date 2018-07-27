9e57a5de01417db87c062a1ebf4a31f0152cee8c
XavierTG <33395394+XavierTG@users.noreply.github.com>
2018-07-27 11:05:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,4 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const fs = require('fs');
-const YTDL = require("ytdl-core");
-const PREFIX = "DataCommand-";
-const opusscript = require("opusscript");
 
 
 
