5cd3f91ee267b1f85fe6d7173862b5137e4ee318
XavierTG <33395394+XavierTG@users.noreply.github.com>
2018-07-27 07:17:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', msg => {
           'secret-key': `${secret}`
         }
       });
-      console.log(data)
+      console.log(data[1])
       break;
     case "test":
       msg.reply('this is a response to a test prompt message.');
