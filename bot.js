57e2bf16e7c6ff9d50754ab0c195e45e0ee3031e
XavierTG <33395394+XavierTG@users.noreply.github.com>
2018-07-27 11:03:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ const PREFIX = "DataCommand-";
 const opusscript = require("opusscript");
 const FFMPEG = require('fluent-ffmpeg');
 const axios = require('axios');
-const secret = `${process.env.Secret_Key}`
-const pass = `${process.env.Command_Authorization_Code}`
 console.log(`secret-key: ${secret}`)
 let musicplaying = false;
 var servers = {};
