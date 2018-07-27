d9cc3bf7918fcd42a94e794890995b59f8e69e49
XavierTG <33395394+XavierTG@users.noreply.github.com>
2018-07-27 10:52:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
   };
   let kyberguild = client.guilds.find(`name`, `Kyber Games Community (Official)`);
   let actionlogchannel = kyberguild.channels.find(`name`, `database_action_logs`);
-  if (kyberguild.channels.find(`name`, `database_action_logs`)) {
+  if (kyberguild.channels.find(`name`, `bot_testing`)) {
     console.log("found")
   } else {
     console.log("not found")
